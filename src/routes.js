5b7954a5ebd912c6ac753c7c999d654ba20bd957
Sérgio Neto <sergio.deusdedith@gmail.com>
2019-08-08 10:55:10 -0300

    Cadastro. Autenticação por jwt

--- src/routes.js
+++ src/routes.js
@@ -2,4 +2,10 @@ const express = require("express");
 
 const routes = express.Router();
 
+const UserController = require("./app/controllers/UserController");
+const SessionController = require("./app/controllers/SessionController");
+
+routes.post("/users", UserController.store);
+routes.post("/session", SessionController.store);
+
 module.exports = routes;
